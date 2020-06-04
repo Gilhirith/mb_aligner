fa0fb138cad978d77ca7643c966e3244584e5b87
Gilhirith <gilhirith@gmail.com>
2020-06-04 16:08:47 +0000

    script to run stitching process

---
+++ run_2d_stitching_Primate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python -u ./mb_aligner/scripts/2d_stitch_driver.py --ts_dir /mnt/disks/date_disk/Primate/tiles_Primate_test_wafer_retake_mfov_coordinates -c conf_Susan_C1_test4.yaml -o /mnt/disks/date_disk/Primate/2d_output_Primate_test_wafer_retake_mfov_coordinates
