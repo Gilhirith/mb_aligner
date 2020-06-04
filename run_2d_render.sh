f8277ae1da2219e608ec13e616fded0776dfa380
Gilhirith <gilhirith@gmail.com>
2020-06-04 16:15:22 +0000

    script to render 4k tiles in full resolution for stitched whole sections

--- run_2d_render.sh
+++ run_2d_render.sh
@@ -2,15 +2,4 @@
 
 python -u ./mb_aligner/scripts/3d_render_driver.py /mnt/disks/data_disk/Primate/2d_output_test_wafer_sift_mfov_coordinates/Sec140 -o /mnt/disks/data_disk/Primate/2d_render_full_res_4k_tiles --scale 1 --tile_size 4096 --blend_type LINEAR --invert_image -p 33
 
-python -u ./mb_aligner/scripts/3d_render_driver.py /mnt/disks/data_disk/Primate/2d_output_test_wafer_sift_mfov_coordinates/Sec141 -o /mnt/disks/data_disk/Primate/2d_render_full_res_4k_tiles --scale 1 --tile_size 4096 --blend_type LINEAR --invert_image -p 33
-
-python -u ./mb_aligner/scripts/3d_render_driver.py /mnt/disks/data_disk/Primate/2d_output_test_wafer_sift_mfov_coordinates/Sec142 -o /mnt/disks/data_disk/Primate/2d_render_full_res_4k_tiles --scale 1 --tile_size 4096 --blend_type LINEAR --invert_image -p 33
-
-python -u ./mb_aligner/scripts/3d_render_driver.py /mnt/disks/data_disk/Primate/2d_output_test_wafer_sift_mfov_coordinates/Sec143 -o /mnt/disks/data_disk/Primate/2d_render_full_res_4k_tiles --scale 1 --tile_size 4096 --blend_type LINEAR --invert_image -p 33
-
-python -u ./mb_aligner/scripts/3d_render_driver.py /mnt/disks/data_disk/Primate/2d_output_test_wafer_sift_mfov_coordinates/Sec144 -o /mnt/disks/data_disk/Primate/2d_render_full_res_4k_tiles --scale 1 --tile_size 4096 --blend_type LINEAR --invert_image -p 33
-
-python -u ./mb_aligner/scripts/3d_render_driver.py /mnt/disks/data_disk/Primate/2d_output_test_wafer_sift_mfov_coordinates/Sec145 -o /mnt/disks/data_disk/Primate/2d_render_full_res_4k_tiles --scale 1 --tile_size 4096 --blend_type LINEAR --invert_image -p 33
-
-python -u ./mb_aligner/scripts/3d_render_driver.py /mnt/disks/data_disk/Primate/2d_output_test_wafer_sift_mfov_coordinates/Sec137 -o /mnt/disks/data_disk/Primate/2d_render_full_res_4k_tiles --scale 1 --tile_size 4096 --blend_type LINEAR --invert_image -p 33
 
